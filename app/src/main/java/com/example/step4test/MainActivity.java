85248262ad165c4f501214b5f156b98627e4d9d2
unknown <jackson.valerioti@gmail.com>
2024-11-29 21:51:07 -0800

    renamed Main activity component tree to 'main' to fix error

--- app/src/main/java/com/example/step4test/MainActivity.java
+++ app/src/main/java/com/example/step4test/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.step4test;
 
+import static com.example.step4test.R.id.main;
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
